818984ea8ce642f52135156be42e631cd00671d0
MaximSudorgin <sudorginmaxi008@gmail.com>
2025-09-16 17:59:13 +0300

    Add files via upload

---
+++ labyb2+.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число");
+double w = Convert.ToDouble(Console.ReadLine());
+if ( w > -10 && w < 10)
+{
+    w += 5;
+
+}
+else
+{
+    w += -10;
+}
+Console.WriteLine("Ответ: " +  w);
